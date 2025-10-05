97238b3fa6817dc9d00b11f8d557aca8c153ffdf
Shaher Jamal Eddin <JAS6MU@bosch.com>
2025-10-05 18:36:55 +0200

    chore: add json parser to the server for testing using curl and json data

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.use(express.json()); //used as I'm sending json data in the body of the request
+
 app.get("/home", (req, res) => {
   res.send("Hello world!, this is the start page of your API");
 });
